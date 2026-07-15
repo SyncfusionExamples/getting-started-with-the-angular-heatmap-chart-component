092b7285908663f93e7780fb317cb5bf382f1b1b
Kokila Poovendran <kokila.poovendran@syncfusion.com>
2026-07-15 12:40:44 +0530

    1040273: Addressed the dependabot issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeatMapModule, TooltipService } from '@syncfusion/ej2-angular-heatmap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeatMapModule],
+  imports: [HeatMapModule],
   providers: [TooltipService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
